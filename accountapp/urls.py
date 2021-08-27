6f63083a53b88b9bbcef27b1389660f028b23b3d
think_wisely <hklee128@naver.com>
2021-08-27 20:44:28 +0900

    Update course 26

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -2,9 +2,11 @@
 
 from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
+
+
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
@@ -13,4 +15,5 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='update'),
 ]
